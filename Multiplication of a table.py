169b1181a5af5026ae163012c322cedc0b733363
BHUKYA VEERANNA <104159515+Veeranna07@users.noreply.github.com>
2022-09-29 06:16:28 +0530

    Update Multiplication of a table.py

--- Multiplication of a table.py
+++ Multiplication of a table.py
@@ -1,5 +1,5 @@
 a=int(input('Enter table number :'))
-b=int(input('Enter the number to which table is to printed : '))
+b=int(input('Enter the number to which table has to printed : '))
 i=1
 while i<=b:
     print(a,'x',i,'=',a*i)
